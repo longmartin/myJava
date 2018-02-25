412a16b3e673be5850609117db1d1a89ceabfcae
longweiwei <long152martin@163.com>
2018-02-25 23:03:41 +0800

    java第一天

---
+++ day01/code/02_注释/Demo.java
@@ -0,0 +1,13 @@
+/*
+    注释的作用
+    代码逻辑请清晰
+*/
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("我爱林青霞");
+        System.out.println("择天记");
+        System.out.println("钧天图");
+        System.out.println("你是谁");
+    }
+}    
---
+++ day01/code/02_注释/HelloWorld.java
@@ -0,0 +1,8 @@
+// class说明
+class HelloWorld {
+    /*main入口函数*/
+    public static void main (String[] args) {
+        //输出语句
+        System.out.println("Hello World!");
+    }
+}
